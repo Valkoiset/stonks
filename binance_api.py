9f9af7fca009e33d51887f0aefba00e11ef90f88
Oleksandr Romanchenko <valkoiset@Oleksandrs-MacBook-Pro.local>
2021-02-06 23:16:03 +0100

    get_klines

--- binance_api.py
+++ binance_api.py
@@ -6,6 +6,21 @@ import os
 
 
 load_dotenv()
-BINANCE_API_KEY = os.getenv('BINANCE_API_KEY')
-BINANCE_SECRET_KEY = os.getenv('BINANCE_SECRET_KEY')
+api_key = os.getenv('BINANCE_API_KEY')
+api_secret = os.getenv('BINANCE_SECRET_KEY')
 client = Client(api_key, api_secret)
+
+dogebtc = client.get_klines(
+    symbol='DOGEBTC', interval=Client.KLINE_INTERVAL_1HOUR)
+renbtc = client.get_klines(
+    symbol='RENBTC', interval=Client.KLINE_INTERVAL_1HOUR)
+grtbtc = client.get_klines(
+    symbol='GRTBTC', interval=Client.KLINE_INTERVAL_1HOUR)
+adabtc = client.get_klines(
+    symbol='ADABTC', interval=Client.KLINE_INTERVAL_1HOUR)
+btceur = client.get_klines(
+    symbol='BTCEUR', interval=Client.KLINE_INTERVAL_1HOUR)
+etheur = client.get_klines(
+    symbol='ETHEUR', interval=Client.KLINE_INTERVAL_1HOUR)
+
+print(btceur)
